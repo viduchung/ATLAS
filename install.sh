93cda5a21f4e7e450cd0ecae6e0b794b5329540c
Vi Đức Hùng <105995900+ViDucHung2006@users.noreply.github.com>
2022-12-04 16:16:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ unzip methods.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; chmod 777 
 pip3 install -r requirements.txt
 sudo apt -y install screen
 git pull
+python3 load.py
 python3 main.py
