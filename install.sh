22ca8490fdd31121e2071234443334aa739a984f
Vi Đức Hùng <105995900+ViDucHung2006@users.noreply.github.com>
2022-12-04 14:58:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 unzip methods.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; chmod 777 methods/*
 pip3 install -r requirements.txt
 sudo apt -y install screen
-python3 enc_main.py
+python3 main.py
